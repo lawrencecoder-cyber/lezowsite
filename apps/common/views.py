ba35b8eee1b773d8815a8c658b67eca389c00b9b
Lawrence Coder <lawrencecoder@gmail.com>
2026-06-28 02:54:28 +0300

    changes

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
+rom django.http import JsonResponse
 
 def home_view(request):
-    return render(request, "templates/home.html")
+    return render(request, "home.html")
 
-from django.http import JsonResponse
 
 def health_check(request):
     return JsonResponse({

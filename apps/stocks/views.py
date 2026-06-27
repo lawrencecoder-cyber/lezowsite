9cc30d7907cd2e294a6c5d4d4609424a70647b3f
Lawrence Coder <lawrencecoder@gmail.com>
2026-06-27 23:57:45 +0300

    Fix deployment dependencies and stocks views

--- apps/stocks/views.py
+++ apps/stocks/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Stock
 
 
-def stock_table_view(request):
-
+def stock_list_view(request):
     stocks = Stock.objects.all()
 
     if request.headers.get("HX-Request"):
-        return render(request, "components/stock_table.html", {
-            "stocks": stocks
-        })
+        return render(
+            request,
+            "components/stock_table.html",
+            {"stocks": stocks}
+        )
 
-    return render(request, "stocks/page.html", {
-        "stocks": stocks
-    })
+    return render(
+        request,
+        "stocks/page.html",
+        {"stocks": stocks}
+    )
+
+
+def stock_detail_view(request, symbol):
+    stock = get_object_or_404(
+        Stock,
+        symbol=symbol
+    )
+
+    return render(
+        request,
+        "stocks/detail.html",
+        {
+            "stock": stock
+        }
+    )

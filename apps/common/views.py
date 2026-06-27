c8542b1adb5b4b01d593a7ce0910fc6330f9f7b9
Lawrence Coder <lawrencecoder@gmail.com>
2026-06-28 02:05:41 +0300

    fixx

--- apps/common/views.py
+++ apps/common/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "templates/home.html")
+
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({
+        "status": "ok"
+    })
